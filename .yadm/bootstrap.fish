ffb06b191918481456824e4eaba056c1e5ec2f16
mig4 <42101611+mig4@users.noreply.github.com>
2019-03-10 15:01:54 +0000

    Configuration refresh
    - upgrade to fish 3.0.0
    - upgrade to fisher > 3.0.0 but disable it as it doesn't work correctly
      (some todo points added for follow up)
    - remove no longer necessary or missing plugins:
      - some were only insalled as deps: cprintf, getopts, git_util
      - some are missing after dissolving fisherman
        (jorgebucaran/fisher#446): debug, g2
      - some are no longer necessary: plugin-node-binpath
    - yadm clone can now automatically bootstrap
    - remove vex from bootstrap as this kind of setup is handled elsewhere
      (via Ansible); this also means gcc and pip aren't needed

--- .yadm/bootstrap.fish
+++ .yadm/bootstrap.fish
@@ -1,22 +1,32 @@
 #!/usr/bin/env fish
 
-# pipsi and vex
-
-if test ! -d ~/.local/venvs
-  mkdir -p ~/.local/venvs
-end
-
-if not command -v vex
-  pip install --user vex
-end
-
 # fisherman
 
-if test ! -f ~/.config/fish/functions/fisher.fish
-  curl -fL --create-dirs \
-    -o ~/.config/fish/functions/fisher.fish https://git.io/fisher
-end
+# TODO disable fisher for now, can't get it to work after v3 update:
+#   - jorgebucaran/fisher#514 reinstalls everything every time causing a lot
+#     of churn
+#   - there's some bug where anything piped to `_fisher_parse -R` (like in
+#     `_fisher_commit` or `fisher ls`) returns nothing, causing fisher to
+#     to quite often actually remove everything (specifically it only works
+#     when called with all packages every time, i.e.
+#     `fisher add < ~/.config/fish/fishfile`)
+#   - some packages I used are missing after dissolving fisherman
+#     (jorgebucaran/fisher#446): debug, g2
+#   - it's unclear what the future of fisher is - jorgebucaran/fisher#443
+#     (it appears the author wants to strip it down / turn it into something
+#     rather different with no plugin/dependency management, etc.)
+# TODO look for an alternative to fisher
+#   - there's https://github.com/oh-my-fish/oh-my-fish but I rather preferred
+#     fisher's lean approach and relying on builtin fish functionality rather
+#     than re-inventing it
+#   - or maybe just use a more generic shell-agnostic package manager like
+#     https://ellipsis.sh/ or https://github.com/pearl-core/pearl
+
+#if test ! -f ~/.config/fish/functions/fisher.fish
+#  curl -fL --create-dirs \
+#    -o ~/.config/fish/functions/fisher.fish https://git.io/fisher
+#end
 
 # for some reason when this runs during `docker build` just plain
 # `fisher` command doesn't find the fishfile
-fisher < ~/.config/fish/fishfile
+#fisher add < ~/.config/fish/fishfile
